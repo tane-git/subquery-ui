839bfc27d1404e5642b4545b4d41cd7503502ffe
tane-git <tane.alexander.wilson@gmail.com>
2022-06-03 09:51:55 +1200

    add layout component in component folder, and export it

--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@ import './src/style.less'
 // * there are a few different ways we can export stuff from the library
 
 // * Just export our modified components
-// export * from './src/components'
+export * from './src/components'
 
 // * export some components from antd and export them individually
 // import { Typography, Form } from 'antd'
@@ -22,13 +22,14 @@ import './src/style.less'
 
 // * export all from antd, then override exports with modified components (from components)
 // ! this isn't working...
-export * from 'antd'
+// * it does work if you delete the dist/index.d.ts file
+// export * from 'antd'
 
 // * override exports with modified components
-export {
-  Button,
+// export {
+  // Button,
   // Typography,
-  Text,
+  // Text,
   // Slider,
   // Popover,
-} from './src/components'
+// } from './src/components'
