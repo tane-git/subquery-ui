242cc812326c50af667d4daf905dc699108d3c1d
tane-git <tane.alexander.wilson@gmail.com>
2022-06-16 16:39:49 +1200

    add text and title and default button settings

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,6 +1,12 @@
-import React from "react";
+import React, { FC } from "react";
 import { Button as AntButton, ButtonProps } from "antd";
 
-const Button = (props: ButtonProps) => <AntButton {...props} />;
+// const Button = (props: ButtonProps) => <AntButton {...props} />;
+
+const Button: FC<ButtonProps> = ({
+  type = 'primary',
+  shape = 'round',
+  ...props
+}) => <AntButton type={type} shape={shape} {...props}  />;
 
 export { Button };
---
+++ src/components/Typography/Title/Title.tsx
@@ -0,0 +1,5 @@
+import * as React from 'react';
+import styles from './Text.module.less';
+import { Typography } from 'antd';
+import { TextProps } from 'antd/lib/typography/Text';
+const TitleAntd = Typography.Title;
