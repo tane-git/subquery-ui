a7282cede66c33d13e1d0cae27a66d3f9e0d60f1
tane-git <tane.alexander.wilson@gmail.com>
2022-06-22 16:00:56 +1200

    changing button, this might break? wont publish yet

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,12 +1,19 @@
 import React, { FC } from "react";
 import { Button as AntButton, ButtonProps } from "antd";
+import styles from './Button.module.less';
 
 // const Button = (props: ButtonProps) => <AntButton {...props} />;
+interface SqButtonProps extends ButtonProps {
+  sqType?: string;
+}
 
-const Button: FC<ButtonProps> = ({
+const Button: FC<SqButtonProps> = ({
+  sqType = 'none',
   type = 'primary',
   shape = 'round',
   ...props
-}) => <AntButton type={type} shape={shape} {...props}  />;
+}) => (
+  <AntButton type={type} shape={shape} className={styles[sqType]} {...props} />
+)
 
 export { Button };
