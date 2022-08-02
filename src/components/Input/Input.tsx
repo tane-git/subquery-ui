1f62bb8377043f84fe2e0f7879a67289a724c448
tane-git <tane.alexander.wilson@gmail.com>
2022-08-02 13:32:38 +1200

    change input and select back to just  export from antd

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,11 +1,1 @@
-import { Input as InputAntd, InputProps } from 'antd'
-import { FC } from 'react'
-
-export const Input: FC<InputProps> = (props) => {
-  return (
-    <InputAntd
-      autoComplete='off'
-      {...props}
-    />
-  )
-}
+export { Input } from 'antd'
--- src/components/Select/Select.tsx
+++ src/components/Select/Select.tsx
@@ -1,11 +1,1 @@
-import { Select as SelectAntd, SelectProps } from 'antd'
-import { FC } from 'react'
-
-export const Select: FC<SelectProps> = (props) => {
-  return (
-    <SelectAntd
-      getPopupContainer={(trigger) => trigger.parentNode}
-      {...props}
-    />
-  )
-}
+export { Select } from 'antd'
